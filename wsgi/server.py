367f05c45ae282a25afcd3ddf6c9672ab87ccebd
Danh Tran <huudanh.k7@gmail.com>
2016-11-26 23:54:47 +0700

    A

--- wsgi/server.py
+++ wsgi/server.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
-from pymessenger.bot import Bot
 from GamePlay import GamePlay
+import os
+import sys
+import json
+import requests
 
 app = Flask(__name__)
 pyBot = GamePlay()
 
 ACCESS_TOKEN = 'EAAEgy21czGwBAFp51ZCCiO1ZBJEPIGB2ICZAkNxUWBYZBDZA0IXgzzZBTsATlY6WOToywU6ivvq5vb3frBLRGTgjNZAEDLNQDn2DQlZBq19ZC7dHDQKHrJ5KGKZCiFoernZCQtEgupnt6S4XAelJbZBA79uX28cnoigYEsCV7Tlb489QUQZDZD'
 VERIFY_TOKEN = 'Danhth'
-
-bot = Bot(ACCESS_TOKEN)
 
 
 @app.route('/webhook', methods=['GET'])
@@ -34,12 +35,39 @@ def handle_incoming_message():
                 print('server received msg: ' + message)
 
                 return_msg = pyBot.processUserMessage(recipient_id, message)
-                bot.send_text_message(recipient_id, return_msg)
-
+                send_message(recipient_id, return_msg)
                 print('server return msg: ' + return_msg)
             else:
                 pass
     return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print(str(message))
 
 if __name__ == '__main__':
     app.run()
