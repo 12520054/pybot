1030962fdbe8c3736d1588bc362e87b7f1418d99
Danh Tran <huudanh.k7@gmail.com>
2016-11-27 16:32:39 +0700

    add something liek shit

--- setup.py
+++ setup.py
@@ -6,5 +6,14 @@ setup(name='PyBot',
       author='Danh Tran',
       author_email='12520054@gm.uit.edu.vn',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe', 'jsonpickle', 'requests>=2.11.0', 'chatterbot']
+      install_requires=['chatterbot',
+                        'Flask>=0.7.2',
+                        'MarkupSafe',
+                        'jsonpickle',
+                        'requests>=2.11.0',
+                        'jsondatabase>=0.1.1',
+                        'nltk<4.0.0',
+                        'pymongo>=3.3.0,<4.0.0',
+                        'python-twitter>=3.0',
+                        'textblob>=0.11.0,<0.12.0']
       )
